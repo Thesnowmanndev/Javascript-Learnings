64b82f953c87bfe763e0ed1d54bd88cf68f5242e
Kyle Martin <kyle.martin.0819@gmail.com>
2024-01-29 20:41:10 -0500

    feat: Added Objects, and Object Methods

--- Courses/Complete-Javascript-2024/3-Javascript-Fundamentals-Part-2/section-project/script.js
+++ Courses/Complete-Javascript-2024/3-Javascript-Fundamentals-Part-2/section-project/script.js
@@ -199,3 +199,150 @@ const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills);
 console.log(tips);
 console.log(totals);
+
+// Introduction to Objects
+console.log(`\n --- Introduction to Objects ---`);
+
+const myCountry = {
+    country: `United States`,
+    capital: `Washington D.C`,
+    language: `English`,
+    nativeSpeakers: `78%`,
+    population: 8100000000,
+    neighbours: [`Canada`, `Mexico`],
+}; // Object variables are organized alphabetically in memory
+console.log(myCountry);
+
+// Dot vs. Bracket Notation
+console.log(`\n --- Dot vs. Bracket Notation ---`);
+let populationShortened = ``;
+
+const populationFormatted = (population) => {
+    const firstDigit = population.toString()[0]; // Strings are essentially an array of characters
+    const secondDigit = population.toString()[1];
+    const thirdDigit = population.toString()[2];
+    let val = ``;
+    if (population >= 1000000000) {
+        val = `${firstDigit}.${secondDigit} billion`;
+    } else if (population >= 1000000) {
+        val = `${firstDigit}.${secondDigit} million`;
+    } else if (population >= 100000) {
+        val = `${firstDigit}${secondDigit}${thirdDigit}K`;
+    } else if (population >= 10000) {
+        if (thirdDigit !== `0`) {
+            val = `${firstDigit}${secondDigit}.${thirdDigit}K`;
+        } else {
+            val = `${firstDigit}${secondDigit}K`;
+        }
+    } else if (population >= 1000) {
+        if (secondDigit == `0`) {
+            val = `${firstDigit}K`;
+        } else {
+            val = `${firstDigit}.${secondDigit}K`;
+        }
+    } else {
+        val = `${population}`;
+    }
+    return val;
+};
+
+populationShortened = populationFormatted(myCountry.population);
+console.log(
+    `${myCountry.country} has ${populationShortened} people, ${myCountry.nativeSpeakers} of the population speak ${myCountry.language}, ${myCountry.neighbours.length} neighbouring countries, and it's capital is ${myCountry.capital}.`
+);
+
+myCountry.population = myCountry[`population`] + 2000000; // Find a variable via string using bracket notation
+console.log(myCountry.population);
+
+myCountry.population = myCountry.population - 2000000;
+console.log(myCountry.population);
+
+// Object Methods
+console.log(`\n --- Object Methods ---`);
+
+const myCountry2 = {
+    country: `United States`,
+    capital: `Washington D.C`,
+    language: `English`,
+    nativeSpeakers: `78%`,
+    population: 8100000000,
+    neighbours: [`Canada`, `Mexico`],
+
+    describe: function () {
+        // How to create a method inside an object
+        console.log(
+            `${this.country} has ${populationShortened} people, ${this.nativeSpeakers} of the population speak ${this.language}, ${this.neighbours.length} neighbouring countries, and it's capital is ${this.capital}.`
+        );
+    },
+
+    checkIsland: function () {
+        let isIsland = true;
+        if (neighbours.length > 0) {
+            isIsland = false;
+        }
+        return isIsland;
+    },
+};
+
+myCountry2.describe();
+console.log(myCountry2.checkIsland());
+
+// CHALLENGE #3
+// Let's go back to Mark and John comparing their BMIs!
+// This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+// Your tasks:
+// For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+// Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property, and also return it from the method.
+
+// Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+// TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+// 👋 OPTIONAL: You can watch my solution in video format in the next lecture
+// IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+console.log(`\n --- Challenge #3 ---`);
+/* Write your code below. Good luck! 🙂 */
+
+const mark = {
+    firstName: `Mark`,
+    lastName: `Miller`,
+    fullName: `Mark Miller`,
+    height: 1.69,
+    mass: 78,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+};
+
+const john = {
+    firstName: `John`,
+    lastName: `Smith`,
+    fullName: `John Smith`,
+    height: 1.95,
+    mass: 92,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+};
+
+const marksBMI = mark.calcBMI();
+const johnsBMI = john.calcBMI();
+
+if (marksBMI > johnsBMI) {
+    console.log(
+        `${mark.fullName}'s BMI (${marksBMI.toFixed(1)}) is higher than ${
+            john.fullName
+        }'s (${johnsBMI.toFixed(1)})!`
+    );
+} else {
+    console.log(
+        `${john.fullName}'s BMI (${johnsBMI.toFixed(1)}) is higher than ${
+            mark.fullName
+        }'s (${marksBMI.toFixed(1)})!`
+    );
+}
